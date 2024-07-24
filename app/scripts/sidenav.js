87cb6099f4e74458d5b81e2f702dd9499cdbda2e
Fabio <fabiojosue16@gmail.com>
2024-07-24 16:30:03 -0600

    [UPDATE] Stable version

--- app/scripts/sidenav.js
+++ app/scripts/sidenav.js
@@ -10,31 +10,6 @@ function goToLogin() {
     window.location.href = '/app/index.html';
 }
 
-// Function to load sidebar content
-// function loadSidebar() {
-//     fetch('/app/components/sidenav.html')
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok ' + response.statusText);
-//             }
-//             return response.text();
-//         })
-//         .then(data => {
-//             // Sanitize the content by removing injected live-server scripts
-//             const sanitizedData = data.replace(/<!-- Code injected by live-server -->[\s\S]*?<\/script>/g, '');
-//             const sidenavElement = document.getElementById('sidenav');
-//             if (sidenavElement) {
-//                 console.log(sanitizedData);
-//                 sidenavElement.innerHTML = sanitizedData;
-//             } else {
-//                 console.error('Element with id "sidenav" not found.');
-//             }
-//         })
-//         .catch(error => {
-//             console.error('There was a problem with the fetch operation:', error);
-//         });
-// }
-
 function loadSidebar() {
     const sidenavHTML = `
         <!-- Your sidenav HTML content here -->
